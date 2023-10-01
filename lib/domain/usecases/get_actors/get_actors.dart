9cd55e44b4d6a83b9a13fd8016b4429c80ec8da4
Randi <randimaulanaakbar@gmail.com>
2023-10-01 11:16:59 +0700

    feat: add usecase get actors

--- lib/domain/usecases/get_actors/get_actors.dart
+++ lib/domain/usecases/get_actors/get_actors.dart
@@ -1,7 +1,7 @@
 import 'package:flix_movie/data/repositories/movie/movie_repository.dart';
 import 'package:flix_movie/domain/entities/actor/actor.dart';
 import 'package:flix_movie/domain/entities/result/result.dart';
-import 'package:flix_movie/domain/usecases/get_actor/get_actors_param.dart';
+import 'package:flix_movie/domain/usecases/get_actors/get_actors_param.dart';
 import 'package:flix_movie/domain/usecases/usecase.dart';
 
 class GetActor implements UseCase<Result<List<Actor>>, GetActorsParam> {
